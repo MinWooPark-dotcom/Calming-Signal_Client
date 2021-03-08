99375c55213bf6f5159b57d593579a292193f245
Minwoo Park <positive1attraction@gmail.com>
2021-03-08 21:31:31 +0900

    [Add] mypage component

--- src/components/MyPage.js
+++ src/components/MyPage.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React from 'react';
+import Nav from './Nav';
+import './MyPage.css';
 
 const MyPage = () => {
-  return <div>mypage</div>;
+  return (
+    <div className="mypage_container">
+      <Nav></Nav>
+      <div className="mypage_profile_title">개인 정보</div>
+      <div className="mypage_profile_box">
+        <div className="mypage_profile_ID_box">
+          <div className="mypage_profile_ID_box_title">ID</div>
+          <div className="mypage_profile_ID_box_value">gildong</div>
+        </div>
+        <div className="mypage_profile_PW_box">
+          <div className="mypage_profile_PW_box_title">비밀번호</div>
+          <div className="mypage_profile_PW_box_value">비밀번호 암호화</div>
+        </div>
+        <div className="mypage_profile_name_box">
+          <div className="mypage_profile_name_box_title">이름</div>
+          <div className="mypage_profile_name_box_value">홍길동</div>
+        </div>
+        {/* <div className="mypage_profile_pet_box">
+          <div className="mypage_profile_pet_box_title">반려견 이름</div>
+          <div className="mypage_profile_pet_box_value">말티즈</div>
+        </div> */}
+      </div>
+    </div>
+  );
 };
 
 export default MyPage;
