9559b7afc71c71c78d934d7443774ed61c0e0bef
Minwoo Park <positive1attraction@gmail.com>
2021-04-24 20:34:45 +0900

    [Fix] position in css

--- src/components/calming-signal pages/CalmingSignal.js
+++ src/components/calming-signal pages/CalmingSignal.js
@@ -1,13 +1,13 @@
 /* eslint react/prop-types: 0 */
 
-import React from "react";
-import NavConatiner from "../../container/NavContainer";
-import HamburgerContainer from "../../container/HamburgerContainer";
-import Sidebar from "../../components/Sidebar";
-import AboutContainer from "../../container/AboutContainer";
-import about_img from "../../img/about_img.jpg"
-import "../About.css";
-import './CalmingSignal.css'
+import React from 'react';
+import NavConatiner from '../../container/NavContainer';
+import HamburgerContainer from '../../container/HamburgerContainer';
+import Sidebar from '../../components/Sidebar';
+import AboutContainer from '../../container/AboutContainer';
+import about_img from '../../img/about_img.jpg';
+import '../About.css';
+import './CalmingSignal.css';
 
 const CalmingSignal = () => {
   return (
@@ -15,42 +15,43 @@ const CalmingSignal = () => {
       <NavConatiner />
       <Sidebar />
       <div className="calming_signal_text_box">
-        <div className="about_title">
-        Calming Signal
-        </div>
+        <div className="about_title">Calming Signal</div>
         <div className="about_text_body">
           <div className="about_text_first">
-          Calming Signal은 이런 출발점에서 시작했습니다.
+            Calming Signal은 이런 출발점에서 시작했습니다.
           </div>
-          <p/>
+          <p />
           <div className="about_text_second_emphasis">
-            “반려견과 소통할 수 있다면 얼마나 좋을까?” 
+            '반려견과 소통할 수 있다면 얼마나 좋을까?'
           </div>
           <div className="about_text_second">
-          우리는 여자친구,남자친구가 외국이이라면 대화하기 위해서 새로운 언어를 배울것입니다.
+            우리는 여자친구,남자친구가 외국이이라면 대화하기 위해서 새로운
+            언어를 배울것입니다.
           </div>
           <div className="about_text_third">
-          하지만 반려견에게는 반려견의 언어가 아닌, 사람의 언어로만 대화를
-          시도합니다.
+            하지만 반려견에게는 반려견의 언어가 아닌, 사람의 언어로만 대화를
+            시도합니다.
           </div>
           <div className="about_text_forth">
-          반려견은 사람의 언어를 못한다는 이유로 소통을 할 수
-          없다고 생각하기도 합니다.
+            반려견은 사람의 언어를 못한다는 이유로 소통을 할 수 없다고
+            생각하기도 합니다.
           </div>
           <div className="about_text_fifth">
-          사실 그렇지 않습니다. 그들은 꾸준한 신호로 우리에게 말을 걸었을
-          것입니다.
+            사실 그렇지 않습니다. 그들은 꾸준한 신호로 우리에게 말을 걸었을
+            것입니다.
           </div>
           <div className="about_text_sixth">
-          반려견이 보내는 신호를 읽고 그들의 마음을 헤아려 함께 행복하게 살
-          수 있도록 하는 것이<br/>calming signal의 목표입니다.
+            반려견이 보내는 신호를 읽고 그들의 마음을 헤아려 함께 행복하게 살 수
+            있도록 하는 것이
+            <br />
+            calming signal의 목표입니다.
           </div>
         </div>
       </div>
       {/* <div className="about_img_box">
         <img className="about_img" src={about_img} alt="dog"></img>
       </div>   */}
-  </div>
+    </div>
   );
 };
 
