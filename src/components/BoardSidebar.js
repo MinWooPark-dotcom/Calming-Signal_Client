0c2dd95c60609aefbe379d65e6e79d2e6f4db01d
Minwoo Park <positive1attraction@gmail.com>
2021-04-27 14:16:08 +0900

    [Add] weather information

--- src/components/BoardSidebar.js
+++ src/components/BoardSidebar.js
@@ -1,12 +1,12 @@
-import React from "react";
-import { Link, withRouter } from "react-router-dom";
-import "./BoardSidebar.css";
+import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import './BoardSidebar.css';
 
 const BoardSidebar = () => {
   return (
     <div className="board_sidebar_container">
       <Link to="/board/free?page=1">
-      <div className="board_sidebar_container_free">자유게시판</div>
+        <div className="board_sidebar_container_free">자유게시판</div>
       </Link>
       <Link to="/board/small?page=1">
         <div className="board_sidebar_container_small">소형견</div>
@@ -16,6 +16,9 @@ const BoardSidebar = () => {
       </Link>
       <Link to="/board/large?page=1">
         <div className="board_sidebar_container_large">대형견</div>
+      </Link>
+      <Link to="/weather">
+        <div className="board_sidebar_container_weather">산책 날씨</div>
       </Link>
     </div>
   );
